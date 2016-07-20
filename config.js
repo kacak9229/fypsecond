a36c5423c894a72acef96723226f67c1c3220eb5
Naufal <naufal@JavascriptCoder.local>
2016-07-20 17:23:05 +0800

    Initial commit

--- config.js
+++ config.js
@@ -1,13 +1,11 @@
 module.exports = {
-  database: 'mongodb://root:abc123@ds031581.mlab.com:31581/fyp',
-  // database: 'mongodb://localhost/fyp3',
+  database: 'mongodb://localhost/fyp3',
   secret: "This is a SECRET19923123",
 
   facebook: {
-    clientID: process.env.FACEBOOK_ID || '1751483768407951',
-    clientSecret: process.env.FACEBOOK_SECRET || 'b79f7082c78e2ddfc1675a4edaa28e97',
+    clientID: process.env.FACEBOOK_ID || 'ID',
+    clientSecret: process.env.FACEBOOK_SECRET || 'SECRET',
     profileFields: ['emails', 'displayName'],
-    // callbackURL: 'http://localhost:3000/auth/facebook/callback',
-    callbackURL: 'https://fypealearning.herokuapp.com/auth/facebook/callback'
+    callbackURL: 'http://localhost:3000/auth/facebook/callback',
   }
 }
--- server-settings.js
+++ server-settings.js
@@ -1,7 +1,6 @@
 /* Require all the important libraries */
 var express = require('express');
 var flash = require('express-flash');
-var expressValidator = require('express-validator');
 var bodyParser = require('body-parser');
 var logger = require('morgan');
 var session = require('express-session');
@@ -13,10 +12,6 @@ var engine = require('ejs-mate');
 var mongoose = require('mongoose');
 
 var passport = require('passport');
-var passportSocketIo = require("passport.socketio");
-
-var multipart = require('connect-multiparty');
-var multipartMiddleware = multipart();
 
 
 var config = require('./config');
@@ -29,19 +24,6 @@ IO connection
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-
-// S3 config
-var fs = require('fs'),
-    S3FS = require('s3fs'),
-    s3fsImpl = new S3FS('naufals3nodejsbucket', {
-        accessKeyId: 'AKIAIPNE4WAGZ5X5FJTA',
-        secretAccessKey: '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'
-    });
-
-// Create our bucket if it doesn't exist
-s3fsImpl.create();
-
-
 
 // Connect to MongoDB either use Mongolab
 mongoose.connect(config.database, function(err) {
@@ -77,33 +59,6 @@ app.use(function(req, res, next) {
   next();
 });
 
-io.use(passportSocketIo.authorize({
-  cookieParser: cookieParser,       // the same middleware you registrer in express
-  key:          'connect.sid',       // the name of the cookie where express/connect stores its session_id
-  secret:       config.secret,    // the session_secret to parse the cookie
-  store:        sessionStore,        // we NEED to use a sessionstore. no memorystore please
-  success:      onAuthorizeSuccess,  // *optional* callback on success - read more below
-  fail:         onAuthorizeFail,     // *optional* callback on fail/error - read more below
-}));
-
-
-function onAuthorizeSuccess(data, accept){
-  console.log('successful connection to socket.io');
-  // OR
-
-  // If you use socket.io@1.X the callback looks different
-  accept();
-}
-
-function onAuthorizeFail(data, message, error, accept){
-  console.log('failed connection to socket.io:', message);
-  // If you use socket.io@1.X the callback looks different
-  // If you don't want to accept the connection
-  if(error)
-    accept(new Error(message));
-  // this error will be sent to the user as a special error-package
-  // see: http://socket.io/docs/client-api/#socket > error-object
-}
 
 
 /* IO file*/
@@ -116,8 +71,6 @@ require('./routes/main')(app);
 require('./routes/user')(app);
 require('./routes/student')(app);
 require('./routes/teacher')(app, io);
-app.use(multipartMiddleware);
-require('./api/api')(app, s3fsImpl);
 
 
 http.listen(process.env.PORT || 3000, function(err) {
